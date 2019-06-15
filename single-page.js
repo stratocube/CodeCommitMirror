97c8c042003ebdeefbe5c3873c35c9c590cac548
Nicholas Barnes <nickbaxter883@gmail.com>
2019-06-15 11:56:00 -0500

    Adding music, animation, tweaks

--- single-page.js
+++ single-page.js
@@ -1,5 +1,20 @@
 var lastTab = 'puzzles'
 
+function init() {
+    animateBanner();
+}
+
+function animateBanner() {
+    var banner = $('#banner');
+    var speed = 400;
+    var startcolor = '#EEEE00';
+    banner
+        .animate({ 'border-top-color': startcolor, 'border-bottom-color': startcolor }, speed)
+        .animate({ 'border-top-color': 'black', 'border-bottom-color': 'black' }, speed,
+            function() { animateBanner(); }
+        ).delay(200);
+}
+
 function loadTab(tabId) {
     console.log('#' + tabId);
 
